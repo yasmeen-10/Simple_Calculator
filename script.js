11e8a7a24b7f27710224a9c8ceebdc5a80a5c40f
Yasmeen <90993453+Yasmeen1016@users.noreply.github.com>
2023-02-25 19:07:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function view(num) {
+    document.getElementById("result").value += num
+}
+function compute() {
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = Math.floor(y)
+}
+function clr() {
+    document.getElementById("result").value = ""
+}
